fab9b8b779f7173bc7f75e6107f8283a419ffae9
danegleason <114887503+danegleason@users.noreply.github.com>
2023-05-01 15:50:15 -0400

    line detection initialization

---
+++ linedetection.py
@@ -0,0 +1,39 @@
+import cv2
+import numpy as np
+
+# gray
+img = cv2.imread('src/visual_servoing/testtrackimages/track8.png')
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+# blur
+kernel_size = 5
+blur_gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size),0)
+# canny edge detection
+low_threshold = 50
+high_threshold = 150
+edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
+# hough for lines
+rho = 1  # distance resolution in pixels of the Hough grid
+theta = np.pi / 180  # angular resolution in radians of the Hough grid
+threshold = 15  # minimum number of votes (intersections in Hough grid cell)
+min_line_length = 50  # minimum number of pixels making up a line
+max_line_gap = 20  # maximum gap in pixels between connectable line segments
+line_image = np.copy(img) * 0  # creating a blank to draw lines on
+
+# Run Hough on edge detected image
+# Output "lines" is an array containing endpoints of detected line segments
+lines = cv2.HoughLinesP(edges, rho, theta, threshold, np.array([]),
+                    min_line_length, max_line_gap)
+
+for line in lines:
+    for x1,y1,x2,y2 in line:
+        cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),5)
+
+# Draw the lines on the  image
+lines_edges = cv2.addWeighted(img, 0.8, line_image, 1, 0)
+
+cv2.imshow('test',lines_edges)
+# waits for user to press any key
+# (this is necessary to avoid Python kernel form crashing)
+cv2.waitKey(0) 
+# closing all open windows
+cv2.destroyAllWindows()
